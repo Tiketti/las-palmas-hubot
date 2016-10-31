768ae330ab1928a92425e087cfccfa4e57b0995b
Perttu <perttu@gmail.com>
2016-11-01 00:23:50 +0200

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,82 @@
+// process.env.HUBOT_FLOWDOCK_API_TOKEN should be set for Flowdock adapter
+const DROPBOX_TOKEN = process.env.DROPBOX_TOKEN;
+const DARK_SKY_TOKEN = process.env.DARK_SKY_TOKEN;
+
+const request = require('request');
+const moment = require('moment');
+const Dropbox = require('dropbox');
+const _ = require('lodash');
+
+const coords = {
+  lat: 28.117432,
+  long: -15.4746367,
+};
+const box = new Dropbox({ accessToken: DROPBOX_TOKEN });
+
+const getCurrentWeather = () =>
+  new Promise((resolve, reject) => {
+    const forecastEndpoint = `https://api.forecast.io/forecast/${DARK_SKY_TOKEN}/${coords.lat},${coords.long}?units=si`;
+    request(forecastEndpoint, (error, response, body) => {
+      if (error) {
+        console.log(`error fetching weather: ${error}`);
+        reject(error);
+      }
+      resolve(JSON.parse(body).currently.temperature);
+    });
+  });
+
+const getDropboxShareLink = file =>
+  new Promise((resolve, reject) => {
+    box.sharingCreateSharedLinkWithSettings({ path: file.path_lower })
+      .then((linkResult) => {
+        resolve(linkResult.url);
+      })
+      .catch((err) => {
+        console.log(`couldn't create shared link: ${err}`);
+        box.sharingListSharedLinks({ path: file.path_lower, direct_only: true })
+          .then((result) => {
+            resolve(result.links[0].url);
+          })
+          .catch(error => reject(error));
+      });
+  });
+
+const getDropboxContents = () =>
+  new Promise((resolve, reject) => {
+    box.filesListFolder({ path: '' })
+      .then((response) => {
+        const randomFile = _.chain(response.entries).filter({ '.tag': 'file' }).sample().value();
+        getDropboxShareLink(randomFile)
+          .then(url => resolve(url));
+      })
+    .catch((err) => {
+      console.log(`error in ${getDropboxContents()}: ${err}`);
+      reject(err);
+    });
+  });
+
+const logInput = (msg) => {
+  console.log(`${moment().format('YYYY-MM-DD HH:MM:SS')} - heard message from: '${msg.message.user.name}', message: ${msg.message.text}, room ${msg.message.room}`);
+};
+
+module.exports = (robot) => {
+  // listens to all messages in all of Hubot's flows
+  // robot.hear('', (msg) => {
+  //   logInput(msg);
+  // });
+
+  robot.respond('/weather/i', (msg) => {
+    getCurrentWeather()
+      .then(weather => msg.reply(`Weather is currently ${weather} °C`))
+      .catch('Sorry, could get weather');
+  });
+
+  robot.respond('/photo/i', (msg) => {
+    logInput(msg);
+    getDropboxContents().then((link) => {
+      msg.reply(link);
+    });
+  });
+
+  // TODO: countdown
+};
